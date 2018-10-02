579ae29b38ca8b01a56c36e173fbf3f3fc8cf926
mariusgg <marius.g.guei@gmail.com>
2018-10-02 15:18:45 +0100

    methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,5 +4,8 @@ describe "DockingStation" do
   it "docking station variable" do
     expect(docking_station).to eq (DockingStation)
   end
+  it "to release a bike" do
+    expect(release_bike)
+  end
 
 end
