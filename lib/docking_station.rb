22bd32d0aa9ab929aeabf803fbe16015f6f8b0dc
mariusgg <marius.g.guei@gmail.com>
2018-10-02 15:14:29 +0100

    testing variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,8 @@
 class DockingStation
   def initialize(init)
-    @docking_station = init 
+    @docking_station = init
   end
-  def docking_station
+  def station 
     @docking_station = release_bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require "docking_station"
 
 describe "DockingStation" do
+  it "docking station variable" do
+    expect(docking_station).to eq (DockingStation)
+  end
 
 end
